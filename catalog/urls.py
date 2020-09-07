b93fa2a5071103d6e57046ea2836faa92da817fa
Tentenone <ksylvs@ukr.net>
2020-09-07 20:55:02 +0300

    First time-2

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,30 +3,26 @@ from . import views
 import debug_toolbar
 from django.urls import include
 from django.urls import reverse
-from django.conf.urls import url
-import pdb
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('', include('django.contrib.auth.urls')),
     path('books/', views.BookListView.as_view(), name='books'),
-    path('book/(?P<pk>[-\w]+)/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
     path('mybooks/',views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
-    path('borrowed/',views.LoanedBooksAllListView.as_view(), name='all-borrowed'),    
+    path(r'borrowed/',views.LoanedBooksAllListView.as_view(), name='all-borrowed'),    
 ]
 urlpatterns += [  
     path('author/create/', views.AuthorCreate.as_view(), name='author_create'),
-    url('author/update/', views.AuthorUpdateList, name='author_list_for_update'),
-    url('author/delete/', views.AuthorDeleteList, name='author_list_for_delete'),
-    url('author/(?P<pk>\d+)/update/', views.AuthorUpdate.as_view(), name='author_update'),
-    url('author/(?P<pk>\d+)/delete/', views.AuthorDelete.as_view(), name='author_delete'),
+    path('author/update/', views.AuthorUpdateList, name='author_list_for_update'),
+    path('author/delete/', views.AuthorDeleteList, name='author_list_for_delete'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author_update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author_delete'),
    
 ]
-
-# pdb.set_trace()
 
 
 
--- loclibrary/settings.py
+++ loclibrary/settings.py
@@ -81,16 +81,16 @@ WSGI_APPLICATION = 'loclibrary.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
 DATABASES = {
-    'default':{
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+#DATABASES = {
+#    'default':{
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
