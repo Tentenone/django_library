11f1311f0572cc6a9e543b5bd53e9a52fa84e26d
Tentenone <ksylvs@ukr.net>
2020-09-07 17:13:07 +0300

    First time-1

--- loclibrary/settings.py
+++ loclibrary/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'locallibrary.urls'
+ROOT_URLCONF = 'loclibrary.urls'
 
 TEMPLATES = [
     {
@@ -75,7 +75,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'locallibrary.wsgi.application'
+WSGI_APPLICATION = 'loclibrary.wsgi.application'
 
 
 # Database
