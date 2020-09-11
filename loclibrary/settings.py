fe549215cb550f743da3186a6c73a0a939f52479
Tentenone <ksylvs@ukr.net>
2020-09-11 19:43:20 +0300

    Added files and changes required for deployment to heroku

--- loclibrary/settings.py
+++ loclibrary/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Add our new application 
     'catalog.apps.CatalogConfig', #This object was created for us in /catalog/apps.py
+    'loclibrary'
 ]
 
 MIDDLEWARE = [
